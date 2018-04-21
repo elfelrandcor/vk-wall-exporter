037d576bf83a2df718918350b00223101bf3bd93
Juriy Panasevich <elfelrandcor@gmail.com>
2018-04-21 18:53:30 +0300

    Configuration

---
+++ src/config/params.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Juriy Panasevich <juriy.panasevich@gmail.com>
+ */
+
+$dotenv = new Dotenv\Dotenv(dirname(__DIR__, 2));
+$dotenv->load();
+
+return [
+    'db' => [
+        'host' => getenv('DB_HOST'),
+        'user' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
+        'name' => getenv('DB_NAME'),
+    ],
+    'token' => getenv('TOKEN'),
+];
