c2c0f8fc123a264df62025c6a32ed81ea38aaed8
Juriy Panasevich <elfelrandcor@gmail.com>
2018-04-22 14:19:39 +0300

    Running script

--- export.php
+++ export.php
@@ -8,8 +8,14 @@ use JuriyPanasevich\VkWallExporter\Console\Console;
 
 $params = require __DIR__ . '/src/config/params.php';
 try {
+    $args = getopt('g:', ['group:']);
+    if (!$id = $args['g'] ?: $args['group']) {
+        throw new \JuriyPanasevich\VkWallExporter\Exception\InvalidConfigurationException('Id of group is required');
+    }
+    $params['id'] = $id;
+
     $app = new \JuriyPanasevich\VkWallExporter\Application($params);
     echo Console::stringRender($app->run(), Console::FG_GREEN);
-} catch (\Exception $e) {
+} catch (Throwable $e) {
     echo Console::stringRender($e->getMessage(), Console::FG_RED);
 }
